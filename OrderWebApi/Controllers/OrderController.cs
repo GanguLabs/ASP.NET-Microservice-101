474ae9acac34dd420d9939a926e1dd1599e71e1a
Anwesh Gangula <gangulaanwesh@gmail.com>
2022-12-03 17:55:56 +0530

    Add project files.

---
+++ OrderWebApi/Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Operations;
+using OrderWebApi.Models;
+
+namespace OrderWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMongoCollection<Order> _orderCollection;
+        public OrderController()
+        {
+            var dbhost = "localhost";
+            var dbname = "dms_order";
+            var connectionstring = $"mongodb://{dbhost}:27017/{dbname}";
+
+            var mongoUrl = MongoUrl.Create(connectionstring);
+            var mongoClient = new MongoClient(mongoUrl);
+            var database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+
+            _orderCollection = database.GetCollection<Order>("order");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _orderCollection.Find(Builders<Order>.Filter.Empty).ToListAsync();
+        }
+
+        [HttpGet("{orderId}")]
+        public async Task<ActionResult<Order>> GetById(string orderId)
+        {
+            var filterDefinition = Builders<Order>.Filter.Eq(x => x.OrderId, orderId);
+            return await _orderCollection.Find(filterDefinition).SingleOrDefaultAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Order order)
+        {
+            await _orderCollection.InsertOneAsync(order);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(Order order)
+        {
+            var filterdefinition  = Builders<Order>.Filter.Eq(x => x.OrderId, order.OrderId);
+            await _orderCollection.ReplaceOneAsync(filterdefinition, order);
+            return Ok();
+        }
+
+        [HttpDelete("{orderId}")]
+        public async Task<ActionResult> Delete(string orderId)
+        {
+            var filterDefinition = Builders<Order>.Filter.Eq(x => x.OrderId, orderId);
+            await _orderCollection.DeleteOneAsync(filterDefinition);
+
+            return Ok();
+        }
+    }
+}
---
+++ OrderWebApi/Models/OrderDetail.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OrderWebApi.Models
+{
+    public class OrderDetail
+    {
+        [BsonElement("product_id"), BsonRepresentation(BsonType.Int32)]
+        public int ProductId { get; set; }
+        [BsonElement("quantity"), BsonRepresentation(BsonType.Decimal128)]
+        public decimal Quantity { get; set; }
+        [BsonElement("unit_price"), BsonRepresentation(BsonType.Decimal128)]
+        public decimal UnitPrice { get; set; }
+    }
+}
